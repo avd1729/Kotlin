b695094cd865b51fc4fcad93a45c6ca40fd623d5
Aravind <aravind19112003@gmail.com>
2025-05-12 22:49:54 +0530

    basics

--- src/exam/Main.kt
+++ src/exam/Main.kt
@@ -1,4 +1,13 @@
 package exam
 
-class Main {
+fun main(){
+    val name: String = "avd"
+    val num: Int = 10
+//    num = 5
+    var num2 = 10
+    num2 = 7
+    println(name)
+    println(num)
+    println(num2)
+    println("name is $name , num is $num")
 }
